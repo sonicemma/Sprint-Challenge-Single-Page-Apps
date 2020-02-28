b4e6e9d9dc952cbc1888b1b9d7cc7bfb63c8c408
emma <sonicjane2020@gmail.com>
2020-02-28 09:04:47 -0800

    added a search form with functionality

--- src/App.js
+++ src/App.js
@@ -3,13 +3,33 @@ import Header from "./components/Header.js";
 import CharacterList from "./components/CharacterList";
 import WelcomePage from "./components/WelcomePage";
 import SearchForm from "./components/SearchForm";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 export default function App() {
   return (
+    <Router>
     <main>
+      <div>
       <Header />
-      <WelcomePage />
-      <CharacterList />
+      <Link to="/components/WelcomePage/">Homepage</Link>
+      <Link to="/components/CharacterList/">Character Page</Link>
+  
+    <Switch>
+      <Route path="/components/WelcomePage/">
+        <WelcomePage />
+      </Route>
+      <Route path="/components/CharacterList/">
+        <CharacterList />
+        <SearchForm />
+      </Route>
+    </Switch>
+    </div>
     </main>
+    </Router>
   );
 }
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,10 +1,46 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import Axios from "axios";
+import CharacterCard from "./CharacterCard";
 
 export default function SearchForm() {
- 
+    const [data, setData] = useState([]);
+    const [search, setSearch] = useState("");
+  
+    useEffect(() => {
+      Axios.get("https://rickandmortyapi.com/api/character/").then(response => {
+        const character = response.data.results.filter(char =>
+          char.name.toLowerCase().includes(search.toLowerCase())
+        );
+  
+        setData(character);
+      });
+    }, [search]);
+    const handleInputChange = e => {
+      setSearch(e.target.value);
+    };
+
   return (
     <section className="search-form">
-     // Add a search form here
+      <div>
+        <form>
+          <input 
+            id="name"
+            type="text"
+            placeholder="Search"
+            value={search}
+            onChange={handleInputChange}
+          />
+        </form>
+        {data.map(prop => {
+        return (
+          <CharacterCard
+            key={prop.id}
+            name={prop.name}
+            gender={prop.gender}
+          />
+        );
+      })}
+      </div>
     </section>
   );
 }
