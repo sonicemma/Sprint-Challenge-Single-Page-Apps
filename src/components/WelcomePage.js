82c45b09c55ca5c6cc6ac531ac887d43328c0829
emma <sonicjane2020@gmail.com>
2020-02-28 10:10:53 -0800

    Met MVP

--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -1,16 +1,23 @@
 import React from "react";
+import styled from 'styled-components';
+
+const CenterH = styled.header`
+  display: flex;
+  flex-direction: column;
+  align-content: center;
+`
 
 export default function WelcomePage() {
   return (
     <section className="welcome-page">
-      <header>
+      <CenterH>
         <h1>Welcome to the ultimate fan site!</h1>
         <img
           className="main-img"
           src="https://rickandmortyapi.com/api/character/avatar/1.jpeg"
           alt="rick"
         />
-      </header>
+      </CenterH>
     </section>
   );
 }
