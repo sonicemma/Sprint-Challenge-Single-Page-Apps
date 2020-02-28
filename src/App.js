70b51037366bc24fee805be23d83d4d8e9becdc5
emma <sonicjane2020@gmail.com>
2020-02-28 08:23:58 -0800

    Mapped through data, it displays the character's name and gender on the page. Rendered various components.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import Header from "./components/Header.js";
-
+import CharacterList from "./components/CharacterList";
+import WelcomePage from "./components/WelcomePage";
+import SearchForm from "./components/SearchForm";
 
 export default function App() {
   return (
     <main>
       <Header />
+      <WelcomePage />
+      <CharacterList />
     </main>
   );
 }
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,30 @@
 import React, { useEffect, useState } from "react";
+import Axios from "axios";
+import CharacterCard from "./CharacterCard";
 
 export default function CharacterList() {
-  // TODO: Add useState to track data from useEffect
+  const [data, setData] = useState([]);
 
   useEffect(() => {
-    // TODO: Add API Request here - must run in `useEffect`
-    //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+    Axios
+      .get(`https://cors-anywhere.herokuapp.com/https://rickandmortyapi.com/api/character/`)
+      .then(response => {
+        setData(response.data.results);
+        //console.log(response.data.results);
+      })
   }, []);
 
   return (
-    <section className="character-list">
-      <h2>TODO: `array.map()` over your state here!</h2>
-    </section>
+    <div className="character-list">
+      {data.map(data => {
+        return (
+          <CharacterCard 
+            id={data.id}
+            name={data.name}
+            gender={data.gender}
+          />
+        )
+      })}
+    </div>
   );
 }
